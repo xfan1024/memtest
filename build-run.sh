b56eb4414eab312e69a733d73ae25e08a521f1b2
xiaofan <xiaofan@iscas.ac.cn>
2024-12-12 21:40:33 +0800

    update build-run.sh
    Signed-off-by: xiaofan <xiaofan@iscas.ac.cn>

--- build-run.sh
+++ build-run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+cpumodel(){
+    local model
+    model=$(echo $(grep 'model name' /proc/cpuinfo  | head -n 1 | cut -d: -f2))
+    if [ -z "$model" ]; then
+        model="unknown $(nproc) core"
+    fi
+    echo "$(uname -m) | $model"
+}
+
+echo "Kernel:" $(uname -r)
+echo "CPU:   " $(cpumodel)
+echo "RAM:   " $(grep MemTotal /proc/meminfo | awk '{printf "%.1f GB\n", $2/1024/1024}')
+echo
+
 rm -rf build/
 mkdir build/
 for M in 0 1; do
@@ -12,6 +26,7 @@ for T in 0 1; do
     macro="-DMAGIC_NOT_ZERO=$M -DWARMUP_INITMAGIC=$I -DWARMUP_TESTFUNC=$T"
     gcc -O3 -fno-tree-loop-distribute-patterns -DMAGIC_NOT_ZERO=$M -DWARMUP_INITMAGIC=$I -DWARMUP_TESTFUNC=$T memtest.c -o $exe
     $exe
+    echo
 done
 done
 done
